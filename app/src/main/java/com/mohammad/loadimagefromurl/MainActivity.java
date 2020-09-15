d0a4df9ce4f1ae4c7e72474630884726c69cb85f
Mohammad Albaba <albabam9@gmail.com>
2020-09-16 01:32:11 +0300

    Initial commit

---
+++ app/src/main/java/com/mohammad/loadimagefromurl/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mohammad.loadimagefromurl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView=(ImageView)findViewById(R.id.image);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
+
+    }
+}
